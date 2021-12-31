cb9b4db3b8d477274b970f206438433f515d26b5
2637309949@qq.com <2637309949@qq.com>
2021-12-31 19:05:40 +0800

    fix: logger

---
+++ comm/logger/extrace.go
@@ -0,0 +1,16 @@
+package logger
+
+import (
+	"context"
+
+	"github.com/2637309949/micro/v3/service/debug/trace"
+)
+
+func Extract(ctx context.Context) (traceID string, parentSpanID string, isFound bool) {
+	return trace.FromContext(ctx)
+}
+
+func ExtractTraceID(ctx context.Context) string {
+	traceID, _, _ := Extract(ctx)
+	return traceID
+}
--- comm/logger/level.go
+++ comm/logger/level.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"context"
 	"os"
 
 	"github.com/2637309949/micro/v3/service/logger"
@@ -47,54 +48,89 @@ func (l Level) Enabled(lvl Level) bool {
 	return lvl >= l
 }
 
-func Info(args ...interface{}) {
-	DefaultLogger.Log(logger.InfoLevel, args...)
+func ExtraceContext(cxs ...context.Context) map[string]interface{} {
+	if len(cxs) == 0 {
+		return map[string]interface{}{}
+	}
+	return map[string]interface{}{"trace": ExtractTraceID(cxs[0])}
 }
 
-func Infof(template string, args ...interface{}) {
-	DefaultLogger.Logf(logger.InfoLevel, template, args...)
+func Info(args ...interface{}) func(cxs ...context.Context) {
+	return func(cxs ...context.Context) {
+		DefaultLogger.Fields(ExtraceContext(cxs...)).Log(logger.InfoLevel, args...)
+	}
 }
 
-func Trace(args ...interface{}) {
-	DefaultLogger.Log(logger.TraceLevel, args...)
+func Infof(template string, args ...interface{}) func(cxs ...context.Context) {
+	return func(cxs ...context.Context) {
+		DefaultLogger.Fields(ExtraceContext(cxs...)).Logf(logger.InfoLevel, template, args...)
+	}
 }
 
-func Tracef(template string, args ...interface{}) {
-	DefaultLogger.Logf(logger.TraceLevel, template, args...)
+func Trace(args ...interface{}) func(cxs ...context.Context) {
+	return func(cxs ...context.Context) {
+		DefaultLogger.Fields(ExtraceContext(cxs...)).Log(logger.TraceLevel, args...)
+	}
 }
 
-func Debug(args ...interface{}) {
-	DefaultLogger.Log(logger.DebugLevel, args...)
+func Tracef(template string, args ...interface{}) func(cxs ...context.Context) {
+	return func(cxs ...context.Context) {
+		DefaultLogger.Fields(ExtraceContext(cxs...)).Logf(logger.TraceLevel, template, args...)
+	}
 }
 
-func Debugf(template string, args ...interface{}) {
-	DefaultLogger.Logf(logger.DebugLevel, template, args...)
+func Debug(args ...interface{}) func(cxs ...context.Context) {
+	return func(cxs ...context.Context) {
+		DefaultLogger.Fields(ExtraceContext(cxs...)).Log(logger.DebugLevel, args...)
+	}
 }
 
-func Warn(args ...interface{}) {
-	DefaultLogger.Log(logger.WarnLevel, args...)
+func Debugf(template string, args ...interface{}) func(cxs ...context.Context) {
+	return func(cxs ...context.Context) {
+		DefaultLogger.Fields(ExtraceContext(cxs...)).Logf(logger.DebugLevel, template, args...)
+	}
 }
 
-func Warnf(template string, args ...interface{}) {
-	DefaultLogger.Logf(logger.WarnLevel, template, args...)
+func Warn(args ...interface{}) func(cxs ...context.Context) {
+	return func(cxs ...context.Context) {
+		DefaultLogger.Fields(ExtraceContext(cxs...)).Log(logger.WarnLevel, args...)
+	}
 }
 
-func Error(args ...interface{}) {
-	DefaultLogger.Log(logger.ErrorLevel, args...)
+func Warnf(template string, args ...interface{}) func(cxs ...context.Context) {
+	return func(cxs ...context.Context) {
+		DefaultLogger.Fields(ExtraceContext(cxs...)).Logf(logger.WarnLevel, template, args...)
+	}
 }
 
-func Errorf(template string, args ...interface{}) {
-	DefaultLogger.Logf(logger.ErrorLevel, template, args...)
+func Error(args ...interface{}) func(cxs ...context.Context) {
+	return func(cxs ...context.Context) {
+		DefaultLogger.Fields(ExtraceContext(cxs...)).Log(logger.ErrorLevel, args...)
+	}
 }
 
-func Fatal(args ...interface{}) {
-	DefaultLogger.Log(logger.FatalLevel, args...)
-	os.Exit(1)
+func Errorf(template string, args ...interface{}) func(cxs ...context.Context) {
+	return func(cxs ...context.Context) {
+		DefaultLogger.Fields(ExtraceContext(cxs...)).Logf(logger.ErrorLevel, template, args...)
+	}
 }
 
-func Fatalf(template string, args ...interface{}) {
-	DefaultLogger.Logf(logger.FatalLevel, template, args...)
-	os.Exit(1)
+func Fatal(args ...interface{}) func(cxs ...context.Context) {
+	return func(cxs ...context.Context) {
+		DefaultLogger.Fields(ExtraceContext(cxs...)).Log(logger.FatalLevel, args...)
+		os.Exit(1)
+	}
+}
+
+func Fatalf(template string, args ...interface{}) func(cxs ...context.Context) {
+	return func(cxs ...context.Context) {
+		DefaultLogger.Fields(ExtraceContext(cxs...)).Logf(logger.FatalLevel, template, args...)
+		os.Exit(1)
+	}
+}
+
+func Fields(fields map[string]interface{}) logger.Logger {
+	return DefaultLogger.Fields(fields)
 }
 
 // Returns true if the given level is at or lower the current logger level
