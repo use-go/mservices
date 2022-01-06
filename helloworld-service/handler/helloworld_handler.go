2d0c16d648e6451af31e43b480be1142c55d35f0
2637309949@qq.com <2637309949@qq.com>
2022-01-06 10:13:59 +0800

    feat: 调整模板

--- helloworld-service/handler/helloworld_handler.go
+++ helloworld-service/handler/helloworld_handler.go
@@ -20,24 +20,40 @@ func (h *Handler) DeleteInfo(ctx context.Context, req *helloworld.InfoFilter, rs
 
 // UpdateInfo defined TODO
 func (h *Handler) UpdateInfo(ctx context.Context, req *helloworld.Info, rsp *helloworld.Info) error {
+	acc, ok := auth.FromContext(ctx)
+	if ok {
+		logger.Infof(ctx, "%v Do UpdateInfo", acc.Name)
+	}
 	rsp.Name = "Hello " + req.Name
 	return nil
 }
 
 // InsertInfo defined TODO
 func (h *Handler) InsertInfo(ctx context.Context, req *helloworld.Info, rsp *helloworld.Info) error {
+	acc, ok := auth.FromContext(ctx)
+	if ok {
+		logger.Infof(ctx, "%v Do InsertInfo", acc.Name)
+	}
 	rsp.Name = "Hello " + req.Name
 	return nil
 }
 
 // QueryInfoDetail defined TODO
 func (h *Handler) QueryInfoDetail(ctx context.Context, req *helloworld.InfoFilter, rsp *helloworld.Info) error {
+	acc, ok := auth.FromContext(ctx)
+	if ok {
+		logger.Infof(ctx, "%v Do QueryInfoDetail", acc.Name)
+	}
 	rsp.Name = "Hello " + req.Name
 	return nil
 }
 
 // QueryInfo defined TODO
 func (h *Handler) QueryInfo(ctx context.Context, req *helloworld.InfoFilter, rsp *helloworld.InfoList) error {
+	acc, ok := auth.FromContext(ctx)
+	if ok {
+		logger.Infof(ctx, "%v Do QueryInfo", acc.Name)
+	}
 	rsp.Data = append(rsp.Data, &helloworld.Info{})
 	return nil
 }
