180d9df6e1d998b15e071266b8da6e7ab01b7004
2637309949@qq.com <2637309949@qq.com>
2022-01-07 11:16:35 +0800

    fix: Debug

--- comm/trace/debug.go
+++ comm/trace/debug.go
@@ -18,7 +18,10 @@ func Debug(ctx context.Context, action string, req, rsp interface{}) func() {
 	defer logger.Init(logger.WithCallerSkipCount(logger.DefaultCallerSkipCount))
 	logger.Infof(ctx, ">>>>> Received %v request = %v", action, reqStr)
 	return func() {
-		rspByte := json.MustMarshal(rsp.(*bytes.Buffer).Bytes())
+		if rspBuffer, ok := rsp.(*bytes.Buffer); ok {
+			rsp = rspBuffer.Bytes()
+		}
+		rspByte := json.MustMarshal(rsp)
 		rspStr := strings.Replace(strings.Replace(string(rspByte), " ", "", -1), "\n", "", -1)
 		cost := int(time.Since(startTime) / time.Microsecond)
 		logger.Init(logger.WithCallerSkipCount(2))
