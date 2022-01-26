ef259a1c10fffc68c1392e5d421a9a905da44a8b
2637309949@qq.com <2637309949@qq.com>
2022-01-26 15:38:12 +0800

    style: 修改模型

--- helloworld-service/model/info.go
+++ helloworld-service/model/info.go
@@ -1,6 +1,9 @@
 package model
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"time"
+)
 
 func UnmarshalInfo(data []byte) (Info, error) {
 	var r Info
@@ -13,8 +16,11 @@ func (r *Info) Marshal() ([]byte, error) {
 }
 
 type Info struct {
-	Id   uint32 `gorm:"column:id" json:"id"`
-	Name string `gorm:"column:name" json:"name"`
+	Id        uint32     `gorm:"column:id" json:"id"`
+	Name      string     `gorm:"column:name" json:"name"`
+	CreatedAt time.Time  `gorm:"column:created_at" json:"created_at"`
+	UpdatedAt time.Time  `gorm:"column:updated_at" json:"updated_at"`
+	DeletedAt *time.Time `gorm:"column:deleted_at" json:"deleted_at"`
 }
 
 // TableName sets the insert table name for this struct type
