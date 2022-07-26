75a7ac733be91a09518a246406858b2ba6dc78a7
2637309949@qq.com <2637309949@qq.com>
2022-07-26 11:23:22 +0800

    feat: 优化InitDb

--- comm/db/db.go
+++ comm/db/db.go
@@ -20,7 +20,7 @@ type Conf struct {
 }
 
 func InitDb(ctx context.Context, i ...uint64) (*gorm.DB, error) {
-	var db = []Conf{}
+	db := []Conf{}
 	v, err := config.Get("db")
 	if err != nil {
 		return nil, err
@@ -41,12 +41,15 @@ func InitDb(ctx context.Context, i ...uint64) (*gorm.DB, error) {
 		idx = i[0]
 	}
 	c = db[idx]
-	return gorm.Open(mysql.New(mysql.Config{
-		DSN:                       fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8&parseTime=True&loc=Local", c.User, c.Passwd, c.Host, c.Port, c.DB), // data source name
-		DefaultStringSize:         256,                                                                                                              // default size for string fields
-		DisableDatetimePrecision:  true,                                                                                                             // disable datetime precision, which not supported before MySQL 5.6
-		DontSupportRenameIndex:    true,                                                                                                             // drop & create when rename index, rename index not supported before MySQL 5.7, MariaDB
-		DontSupportRenameColumn:   true,                                                                                                             // `change` when rename column, rename column not supported before MySQL 8, MariaDB
-		SkipInitializeWithVersion: false,                                                                                                            // auto configure based on currently MySQL version
-	}), &gorm.Config{})
+
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8&parseTime=True&loc=Local", c.User, c.Passwd, c.Host, c.Port, c.DB)
+	cfg := mysql.Config{
+		DSN:                       dsn,   // data source name
+		DefaultStringSize:         256,   // default size for string fields
+		DisableDatetimePrecision:  true,  // disable datetime precision, which not supported before MySQL 5.6
+		DontSupportRenameIndex:    true,  // drop & create when rename index, rename index not supported before MySQL 5.7, MariaDB
+		DontSupportRenameColumn:   true,  // `change` when rename column, rename column not supported before MySQL 8, MariaDB
+		SkipInitializeWithVersion: false, // auto configure based on currently MySQL version
+	}
+	return gorm.Open(mysql.New(cfg), &gorm.Config{})
 }
