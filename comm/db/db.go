efe203a558d6c8a82e7773cc0ef9bf79a14bf53a
2637309949@qq.com <2637309949@qq.com>
2022-07-26 15:10:37 +0800

    fix: 修改InitDb

--- comm/db/db.go
+++ comm/db/db.go
@@ -3,13 +3,17 @@ package db
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	"comm/config"
-	"comm/errors"
+	"comm/logger"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
+
+var dbs []*gorm.DB
+var once sync.Once
 
 type Conf struct {
 	User   string `json:"user"`
@@ -19,37 +23,50 @@ type Conf struct {
 	DB     string `json:"db"`
 }
 
+func scan(ctx context.Context) func() {
+	return func() {
+		cfs := []Conf{}
+		v, err := config.Get("db")
+		if err != nil {
+			return
+		}
+
+		err = v.Scan(&cfs)
+		if err != nil {
+			return
+		}
+
+		if len(dbs) == 0 {
+			logger.Errorf(ctx, "not found db")
+			return
+		}
+
+		for i := range cfs {
+			v := cfs[i]
+			dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8&parseTime=True&loc=Local", v.User, v.Passwd, v.Host, v.Port, v.DB)
+			logger.Infof(ctx, "%v", dsn)
+			db, err := gorm.Open(mysql.New(mysql.Config{
+				DSN:                       dsn,   // data source name
+				DefaultStringSize:         256,   // default size for string fields
+				DisableDatetimePrecision:  true,  // disable datetime precision, which not supported before MySQL 5.6
+				DontSupportRenameIndex:    true,  // drop & create when rename index, rename index not supported before MySQL 5.7, MariaDB
+				DontSupportRenameColumn:   true,  // `change` when rename column, rename column not supported before MySQL 8, MariaDB
+				SkipInitializeWithVersion: false, // auto configure based on currently MySQL version
+			}), &gorm.Config{})
+			if err != nil {
+				logger.Errorf(ctx, "%v", err)
+				continue
+			}
+			dbs = append(dbs, db)
+		}
+	}
+}
+
 func InitDb(ctx context.Context, i ...uint64) (*gorm.DB, error) {
-	db := []Conf{}
-	v, err := config.Get("db")
-	if err != nil {
-		return nil, err
-	}
-	err = v.Scan(&db)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(db) == 0 {
-		return nil, errors.InternalServerError("db not found")
-	}
-
-	c := Conf{}
+	once.Do(scan(ctx))
 	idx := uint64(0)
-
-	if len(i) > 0 {
+	if len(i) > 0 && len(dbs) >= int(i[0]) {
 		idx = i[0]
 	}
-	c = db[idx]
-
-	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8&parseTime=True&loc=Local", c.User, c.Passwd, c.Host, c.Port, c.DB)
-	cfg := mysql.Config{
-		DSN:                       dsn,   // data source name
-		DefaultStringSize:         256,   // default size for string fields
-		DisableDatetimePrecision:  true,  // disable datetime precision, which not supported before MySQL 5.6
-		DontSupportRenameIndex:    true,  // drop & create when rename index, rename index not supported before MySQL 5.7, MariaDB
-		DontSupportRenameColumn:   true,  // `change` when rename column, rename column not supported before MySQL 8, MariaDB
-		SkipInitializeWithVersion: false, // auto configure based on currently MySQL version
-	}
-	return gorm.Open(mysql.New(cfg), &gorm.Config{})
+	return dbs[idx], nil
 }
